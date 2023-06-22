de4ebf7dd579584910d54eac82e3709331d8ab13
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-06-22 19:23:28 +0300

    Update KIM3_TASK1.cpp

--- KIM3_TASK1.cpp
+++ KIM3_TASK1.cpp
@@ -3,7 +3,7 @@
 // которые есть во всех строках файла и обладают заданным свойством. Функция должна 
 // принимать лямбду для проверки нужного свойства слова. Привести пример вызова функции
 // для свойства: оканчиваются заданной буквой
- 
+
 #include <iostream>
 #include <functional>
 #include <fstream>
@@ -11,12 +11,13 @@
 #include <string>
 #include <sstream>
 #include <Windows.h>
- 
+#include <set>
+
 auto lambda = [](std::string word, char letter)
 {
 	return word[word.length() - 1] == letter;
 };
- 
+
 void task(std::ifstream& file, bool (*lambda)(std::string, char), char letter)
 {
 	std::map<std::string, int> dict;
@@ -26,19 +27,24 @@ void task(std::ifstream& file, bool (*lambda)(std::string, char), char letter)
 	while (getline(file, str))
 	{
 		++count;
+		std::set<std::string> curr;
 		std::istringstream iss(str);
 		while (iss >> word)
 		{
 			if (lambda(word, letter))
 			{
-				if (dict.find(word) == dict.end())
-				{
-					dict.insert({ word, 1 });
-				}
-				else
-				{
-					++dict.find(word)->second;
-				}
+				curr.insert(word);
+			}
+		}
+		for (auto& i : curr)
+		{
+			if (dict.find(i) == dict.end())
+			{
+				dict.insert({ i, 1 });
+			}
+			else
+			{
+				dict.find(i)->second++;
 			}
 		}
 	}
@@ -48,7 +54,7 @@ void task(std::ifstream& file, bool (*lambda)(std::string, char), char letter)
 			std::cout << i.first << std::endl;
 	}
 }
- 
+
 void main()
 {
 	SetConsoleCP(1251);
@@ -56,6 +62,6 @@ void main()
 	std::ifstream file("input.txt");
 	char letter;
 	std::cin >> letter;
- 
+
 	task(file, lambda, letter);
 }
