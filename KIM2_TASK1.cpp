c95ed271c92eff3f6cc65ea1191a21b2aa38b892
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-06-22 19:24:37 +0300

    Update KIM2_TASK1.cpp

--- KIM2_TASK1.cpp
+++ KIM2_TASK1.cpp
@@ -1,9 +1,9 @@
 // Дан файл, состоящий из нескольких строк. Каждая строка содержит слова,
 // разделенные любым количесвтом пробелов. Разработать функцию для поиска слов,
-// которые встречаются в файле один раз и обладают заданным свойством. Функция должна 
+// которые есть во всех строках файла и обладают заданным свойством. Функция должна 
 // принимать лямбду для проверки нужного свойства слова. Привести пример вызова функции
 // для свойства: оканчиваются заданной буквой
- 
+
 #include <iostream>
 #include <functional>
 #include <fstream>
@@ -11,12 +11,13 @@
 #include <string>
 #include <sstream>
 #include <Windows.h>
- 
+#include <set>
+
 auto lambda = [](std::string word, char letter)
 {
 	return word[word.length() - 1] == letter;
 };
- 
+
 void task(std::ifstream& file, bool (*lambda)(std::string, char), char letter)
 {
 	std::map<std::string, int> dict;
@@ -24,19 +25,24 @@ void task(std::ifstream& file, bool (*lambda)(std::string, char), char letter)
 	std::string word;
 	while (getline(file, str))
 	{
+		std::set<std::string> curr;
 		std::istringstream iss(str);
 		while (iss >> word)
 		{
 			if (lambda(word, letter))
 			{
-				if (dict.find(word) == dict.end())
-				{
-					dict.insert({ word, 1 });
-				}
-				else
-				{
-					++dict.find(word)->second;
-				}
+				curr.insert(word);
+			}
+		}
+		for (auto& i : curr)
+		{
+			if (dict.find(i) == dict.end())
+			{
+				dict.insert({ i, 1 });
+			}
+			else
+			{
+				dict.find(i)->second++;
 			}
 		}
 	}
@@ -46,7 +52,7 @@ void task(std::ifstream& file, bool (*lambda)(std::string, char), char letter)
 			std::cout << i.first << std::endl;
 	}
 }
- 
+
 void main()
 {
 	SetConsoleCP(1251);
@@ -54,6 +60,6 @@ void main()
 	std::ifstream file("input.txt");
 	char letter;
 	std::cin >> letter;
- 
+
 	task(file, lambda, letter);
 }
