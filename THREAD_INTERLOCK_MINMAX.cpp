504a0830135022308e38f3d297e6dc8150dd01f4
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-06-22 20:55:03 +0300

    Create THREAD_INTERLOCK_MINMAX.cpp

---
+++ THREAD_INTERLOCK_MINMAX.cpp
@@ -0,0 +1,70 @@
+//КИМ 3 
+//4. Дан одномерный массив целых чисел. Проверить, что количество четных элементов кратно трем. 
+//Создание потоков организовать с использованием класса thread. Потоки взаимодействующие. Взаимодействие организовать через объект ядра "Событие"
+
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <fstream>
+
+const size_t NTHREAD = 4;
+size_t n = 0;
+volatile long res_max = 0;
+volatile long lock = false;
+
+//cosnt
+void count_num(int* arr, int beg, int end)
+{
+	int local_max = 0;
+	for (int i = beg; i != end; i++)
+	{
+		if (arr[i] > local_max) //меняется только это условие в зависимости от задачи
+		{
+			local_max = arr[i];
+		}
+	}
+
+	while (_InterlockedExchange(&lock, true))
+	{
+		Sleep(0);
+	}
+	if (local_max > res_max)
+	{
+		res_max = local_max;
+	}
+	_InterlockedExchange(&lock, false);
+}
+
+//const
+void par_count_num(int* arr)
+{
+	std::thread TH[NTHREAD - 1];
+	size_t size = n / NTHREAD;
+
+	for (size_t i = 0; i < NTHREAD - 1; i++)
+	{
+		TH[i] = std::thread(count_num, arr, i * size, (i + 1) * size);
+	}
+
+	count_num(arr, (NTHREAD - 1) * size, n);
+
+	for (int i = 0; i < NTHREAD - 1; i++)
+	{
+		TH[i].join();
+	}
+}
+
+int main()
+{
+	int* arr;
+	std::fstream file("input.txt");
+	file >> n;
+	arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		file >> arr[i];
+	}
+	
+	par_count_num(arr);
+	std::cout << "Max: " << res_max << std::endl;
+}
